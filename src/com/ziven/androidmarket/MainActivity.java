7aa7910a0b219ece4c08a5517232468713d305ec
zivensonice <zivensonice@163.com>
2015-02-28 16:13:09 +0800

    dd

--- src/com/ziven/androidmarket/MainActivity.java
+++ src/com/ziven/androidmarket/MainActivity.java
@@ -1,7 +1,6 @@
 package com.ziven.androidmarket;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.Menu;
 
