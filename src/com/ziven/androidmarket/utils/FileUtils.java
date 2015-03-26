050aaf199bcb9225756bff20e50d2cb4095d446a
zivensonice <zivensonice@163.com>
2015-03-26 18:59:12 +0800

    修改数据加载bug

--- src/com/ziven/androidmarket/utils/FileUtils.java
+++ src/com/ziven/androidmarket/utils/FileUtils.java
@@ -1,7 +1,7 @@
 package com.ziven.androidmarket.utils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -148,4 +148,34 @@ public class FileUtils {
 		return res;
 	}
 
+	/** 改名 */
+	public static boolean copy(String src, String des, boolean delete) {
+		File file = new File(src);
+		if (!file.exists()) {
+			return false;
+		}
+		File desFile = new File(des);
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		try {
+			in = new FileInputStream(file);
+			out = new FileOutputStream(desFile);
+			byte[] buffer = new byte[1024];
+			int count = -1;
+			while ((count = in.read(buffer)) != -1) {
+				out.write(buffer, 0, count);
+				out.flush();
+			}
+		} catch (Exception e) {
+			L.e(e);
+			return false;
+		} finally {
+			IOUtils.close(in);
+			IOUtils.close(out);
+		}
+		if (delete) {
+			file.delete();
+		}
+		return true;
+	}
 }
