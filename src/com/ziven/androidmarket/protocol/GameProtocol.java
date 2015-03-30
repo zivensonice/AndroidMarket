8ff7b3e15d35e6693ca28105b04fc68be8ca49d7
zivensonice <zivensonice@163.com>
2015-03-30 18:52:03 +0800

    GameInfo AppInfo

---
+++ src/com/ziven/androidmarket/protocol/GameProtocol.java
@@ -0,0 +1,29 @@
+package com.ziven.androidmarket.protocol;
+
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+import com.ziven.androidmarket.utils.L;
+import com.ziven.bean.AppInfo;
+
+public class GameProtocol extends BaseProtocol<List<AppInfo>> {
+
+	@Override
+	public String getKey() {
+		return "game";
+	}
+
+	@Override
+	public List<AppInfo> parseFromJson(String jsonStr) {
+		try {
+			Gson gson = new Gson();
+			return gson.fromJson(jsonStr, new TypeToken<List<AppInfo>>() {
+			}.getType());
+		} catch (JsonSyntaxException e) {
+			L.e(e);
+			return null;
+		}
+	}
+}
